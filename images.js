e85b68bf41a07ab423fc39328b10434d1d0037a4
radarpok <vdiana.m8@gmail.com>
2020-12-16 16:59:46 +0100

    creating flipcards for the plant learning app

---
+++ images.js
@@ -0,0 +1,3 @@
+const images = {
+        url: "https://mega.nz/folder/bwM0wBbb#--hTkrTUNbOkRN4qnBM4ow"
+}
---
+++ kartyak.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    var element = document.body;
+    element.ClassList.toggle("dark-mode");
+}
